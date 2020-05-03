fe997685d267275b32f17ad26cec7c61c9101c18
Saran RTS <rtssaran@gmail.com>
2020-05-03 22:03:18 +0530

    New screen added - Staking

--- screens/Staking.js
+++ screens/Staking.js
@@ -1,11 +1,58 @@
 /* eslint-disable prettier/prettier */
 import React, { Component } from 'react';
+import { StyleSheet, Image, View, TouchableOpacity, Text, StatusBar, Platform, ScrollView, Alert, ActivityIndicator, RefreshControl} from 'react-native';
+import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+import { Icon } from 'react-native-elements';
 
+const STATUS_BAR_HEIGHT = Platform.OS === 'ios' ? 40 : StatusBar.currentHeight;
 export default class Staking extends Component {
     render() {
+        const styles = StyleSheet.create({
+			statusBar: {
+                width: '100%',
+                backgroundColor: '#070E18',
+                height: STATUS_BAR_HEIGHT,
+			},
+			section: {
+                backgroundColor: '#060E17',
+                minHeight: '100%',
+                flex: 1,
+			},
+			header: {
+                flexDirection: 'row',
+                marginTop: -10,
+			},
+			icon: {
+				marginLeft: wp('5%'),
+                padding: 5,
+                transform: [
+                    { rotate: '-90deg' },
+                    { rotateX: '180deg' },
+                ],
+			},
+			title: {
+				color: 'white',
+				fontFamily: 'Armegoe',
+				textAlign: 'center',
+				alignSelf: 'center',
+				fontSize: 20,
+				marginLeft: wp('25%'),
+            },
+		});
+
+        const { statusBar, section, header, icon, title } = styles;
+        const { navigation } = this.props;
         return (
             <>
-
+                <View style = {statusBar}>
+                    <StatusBar barStyle = "light-content" backgroundColor = "#060E17"/>
+                </View>
+				<View style = {section}>
+					<View style = {header}>
+						<Icon type = "feather" name = "bar-chart" color = "#fff" size = {wp('9.5%')} iconStyle = {icon} onPress = {() => navigation.openDrawer()} underlayColor = "transparent" />
+						<Text style = {title}>Staking</Text>
+                    </View>
+                </View>
             </>
         );
     }
